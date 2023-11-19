b8eb6fe4d487e9915edd970fafaa0efabe96040b
abyzt755 <113474651+abyzt775@users.noreply.github.com>
2023-11-18 19:04:56 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+
+        // Escribe el texto en terminal y hace un salto de linea
+        System.out.println("Hola Mundo\n");
+
+    }
+
+}
